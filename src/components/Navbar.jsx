5cb1d2788f88d670e698f5f5737c956f6f3c80df
Razzakowais28 <91192838+Razzakowais28@users.noreply.github.com>
2025-07-01 10:54:51 +0000

    Replace react-tilt with react-parallax-tilt and fix build

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -44,8 +44,8 @@ const Navbar = () => {
         >
           <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
           <p className='text-white text-[18px] font-bold cursor-pointer flex '>
-            Adrian &nbsp;
-            <span className='sm:block hidden'> | JavaScript Mastery</span>
+            Mohammed Abdul Razzak Owais &nbsp;
+            <span className='sm:block hidden'> | Full-Stack Developer / Automation Specialist</span>
           </p>
         </Link>
 
--- src/constants/index.js
+++ src/constants/index.js
@@ -22,7 +22,7 @@ import {
   carrent,
   jobit,
   tripguide,
-  threejs,
+  threejs
 } from "../assets";
 
 export const navLinks = [
@@ -46,7 +46,7 @@ const services = [
     icon: web,
   },
   {
-    title: "React Native Developer",
+    title: "Automation Developer",
     icon: mobile,
   },
   {
@@ -54,7 +54,7 @@ const services = [
     icon: backend,
   },
   {
-    title: "Content Creator",
+    title: "Content Developer",
     icon: creator,
   },
 ];
